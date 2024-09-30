01260c9b78eebbdfdced1d8047672635e32e9a83
beijihu <jun.yin@live.com>
2024-09-30 10:40:27 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,9 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'source.unsplash.com',
       'p1.qhimg.com',
-   
+      'webmention.io',
+      'ko-fi.com',
+      'amazonaws.com',
     ]
   },
   // 默认将feed重定向至 /public/rss/feed.xml
